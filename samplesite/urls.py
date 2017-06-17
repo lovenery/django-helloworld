20c54ac895a82b771b23faec6b416cd418612622
Hsu <edittye379@gmail.com>
2017-06-18 04:55:04 +0800

    add polls app!

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from samplesite.views import hello_world, root_page, random_number
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello_world),
     url(r'^$', root_page),
-    url(r'^random/(\d+)$', random_number)
+    url(r'^random/(\d+)$', random_number),
+    url(r'^polls/', include('polls.urls')),
 ]
