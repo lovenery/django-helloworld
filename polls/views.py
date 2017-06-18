8c73ff73e32c358acbdca0ec2298f7285cf1bd5b
Hsu <edittye379@gmail.com>
2017-06-19 01:56:06 +0800

    finish all test cases

--- polls/views.py
+++ polls/views.py
@@ -28,7 +28,9 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
