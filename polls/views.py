4982b9b774f5a84595d77ff41cdf1eac60fcfd1b
Hsu <edittye379@gmail.com>
2017-06-18 22:37:15 +0800

    generic, first test

--- polls/views.py
+++ polls/views.py
@@ -3,24 +3,43 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from .models import Question, Choice
 
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'lastest_question_list': lastest_question_list,
-    }
-    # output = ', '.join([q.question_text for q in lastest_question_list])
-    # return HttpResponse(output)
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'lastest_question_list': lastest_question_list,
+#     }
+#     # output = ', '.join([q.question_text for q in lastest_question_list])
+#     # return HttpResponse(output)
+#     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'lastest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
